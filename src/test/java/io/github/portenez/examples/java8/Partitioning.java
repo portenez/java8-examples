7dcc2124b5e1256b5e0523d07b898037b9edc25e
Victor Garcia <victor.garcia@verato.com>
2016-01-01 11:58:33 -0500

    initial commit

---
+++ src/test/java/io/github/portenez/examples/java8/Partitioning.java
@@ -0,0 +1,18 @@
+package io.github.portenez.examples.java8;
+
+
+import org.junit.Test;
+
+/**
+ * Created by vgarcia on 1/1/16.
+ */
+public class Partitioning {
+
+
+    @Test
+    public void partitioning() {
+
+
+
+    }
+}
