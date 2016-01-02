d3ff662ee8ff3bcc3a042785f53f0bcd619108d7
Victor Garcia <victor.garcia@verato.com>
2016-01-02 00:57:42 -0500

    added grouping examples with map type and group collector

--- functional-programming/src/test/java/io/github/portenez/examples/java8/functional/GroupingWithCollectors.java
+++ functional-programming/src/test/java/io/github/portenez/examples/java8/functional/GroupingWithCollectors.java
@@ -2,14 +2,15 @@ package io.github.portenez.examples.java8.functional;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
 
 /**
  * Examples of how to use grouping
@@ -30,5 +31,54 @@ public class GroupingWithCollectors {
         assertThat(grouped.get(3), is(Arrays.asList("one", "two", "six", "ten")));
         assertThat(grouped.get(4), is(Arrays.asList("four", "five", "nine")));
         assertThat(grouped.get(5), is(Arrays.asList("three", "seven", "eight")));
+    }
+
+    @Test
+    public void groupStringsByLengthInTreeMap() {
+
+        final Map<Character, List<String>> grouped =
+                Stream.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+                    .collect(
+                            Collectors.groupingBy(
+                                    s-> s.charAt(0),
+                                    TreeMap::new,
+                                    Collectors.toList()
+                            ));
+
+
+        assertThat(grouped.keySet(), contains('e','f','n','o','s','t'));
+
+        assertThat(grouped.get('e'), is(Arrays.asList("eight")));
+        assertThat(grouped.get('f'), is(Arrays.asList("four","five")));
+        assertThat(grouped.get('n'), is(Arrays.asList("nine")));
+        assertThat(grouped.get('o'), is(Arrays.asList("one")));
+        assertThat(grouped.get('s'), is(Arrays.asList("six", "seven")));
+        assertThat(grouped.get('t'), is(Arrays.asList("two","three","ten")));
+
+    }
+
+    @Test
+    public void groupStringsByLengthInTreeMapWithSets() {
+
+        final Map<Character, Set<String>> grouped =
+                Stream.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten")
+                        .collect(
+                                Collectors.groupingBy(
+                                        s-> s.charAt(0),
+                                        TreeMap::new,
+                                        Collectors.toCollection(TreeSet::new)
+                                ));
+
+
+        assertThat(grouped.keySet(), contains('e','f','n','o','s','t'));
+
+
+        assertThat(grouped.get('e'), contains("eight"));
+        assertThat(grouped.get('f'), contains("five", "four"));
+        assertThat(grouped.get('n'), contains("nine"));
+        assertThat(grouped.get('o'), contains("one"));
+        assertThat(grouped.get('s'), contains( "seven", "six"));
+        assertThat(grouped.get('t'), contains("ten", "three", "two"));
+
     }
 }
